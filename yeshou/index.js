1ed6fc2f324e182bb124dc3ffc2d4d37777757d8
jiahua <jiahua.chu@nusri.cn>
2020-04-15 19:18:04 +0800

    v2

--- yeshou/index.js
+++ yeshou/index.js
@@ -3,9 +3,14 @@ var interval = 500;
 var pressCount = 0;
 // var playing = false;
 var combo = { count: 0 };
+
+document.addEventListener('touchstart', shout, false);
+
+
 Object.defineProperty(combo,'count',{
     set: function (value) {
         document.getElementById('combo').innerText = '臭气值：' + value;
+        document.getElementById('combo').style.fontSize = Math.max(value * 2, 16)+'px';
         audio.volume = Math.max(Math.min(value / 30, 1), 0);
     }
 })
